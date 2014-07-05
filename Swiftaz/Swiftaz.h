3c0dc49f6340aef396f33a545ea4748851d1f85f
Tomek Cejner <tomek@smartrecruiters.com>
2014-07-05 14:13:43 +0200

    Initial Commit

---
+++ Swiftaz/Swiftaz.h
@@ -0,0 +1,19 @@
+//
+//  Swiftaz.h
+//  Swiftaz
+//
+//  Created by Tomek on 05.07.2014.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swiftaz.
+FOUNDATION_EXPORT double SwiftazVersionNumber;
+
+//! Project version string for Swiftaz.
+FOUNDATION_EXPORT const unsigned char SwiftazVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftaz/PublicHeader.h>
+
+
